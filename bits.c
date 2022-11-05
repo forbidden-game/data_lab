d32ec892ef4fbea1cb47256fa71350bce787cc31
Fg <panxiezhao@gmail.com>
2022-11-06 02:10:52 +0800

    conditional

--- bits.c
+++ bits.c
@@ -220,7 +220,8 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+	int t = ~(!x) + 1;
+	return (~t & y) | (t & z);
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
